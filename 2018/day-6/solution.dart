176ce02c48f254d0e8150ab4cd3518e8b29ef5c5
Lauren Zou <zou.lauren@gmail.com>
2018-12-05 22:40:01 -0500

    Add Dart skeleton code for day 6.

---
+++ 2018/day-6/solution.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void part1(String input) {
+  print('Part 1');
+}
+
+void part2(String input) {
+  print('Part 2');
+}
+
+void main() {
+  final input = new File('input.txt').readAsStringSync();
+  part1(input);
+  part2(input);
+}
---
+++ 2018/day-6/test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('Part 1', () {
+    test('', () {
+    });
+  });
+
+  group('Part 2', () {
+    test('', () {
+    });
+  });
+}
