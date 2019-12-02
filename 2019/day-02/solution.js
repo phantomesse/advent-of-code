e33472bcd78b6f91420d29fd23b10770683ea991
Lauren Zou <zou.lauren@gmail.com>
2019-12-01 22:50:44 -0500

    Add skeleton for day 2.

---
+++ 2019/day-02/solution.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const fs = require('fs');
+
+// Get input.
+const inputs = fs
+  .readFileSync('input.txt', 'utf8')
+
+// Part 1.
+let answer;
+console.log('Part 1: ' + answer);
+
+
+// Part 2.
+console.log('Part 2: ' + answer);
+
