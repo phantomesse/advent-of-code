2d93c38629d2a103b8746c998a5e96e91ca4d1a2
Lauren Zou <zou.lauren@gmail.com>
2019-12-03 23:08:31 -0500

    Add typescript scaffolding and skeleton for day 4.

---
+++ 2019/day-04/solution.ts
@@ -0,0 +1,13 @@
+import { readFileSync } from 'fs';
+
+function main() {
+  let input = readFileSync('input.txt', 'utf8');
+
+  // Part 1.
+  console.log(`part 1: `);
+
+  // Part 2.
+  console.log(`part 2: `);
+}
+
+if (require.main === module) main();
---
+++ 2019/day-04/test.ts
@@ -0,0 +1,14 @@
+import {} from './solution';
+import { expect } from 'chai';
+
+describe('part 1', function() {
+  it('', function() {
+    expect(true).to.equal(true);
+  });
+});
+
+describe('part 2', function() {
+  it('', function() {
+    expect(true).to.equal(true);
+  });
+});
