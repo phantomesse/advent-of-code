031a3342b8479df84fc47e629bb6343818b490cb
Lauren Zou <zou.lauren@gmail.com>
2019-12-04 00:17:36 -0500

    Solved day 4.

--- 2019/day-04/solution.ts
+++ 2019/day-04/solution.ts
@@ -1,13 +1,57 @@
 import { readFileSync } from 'fs';
 
-function main() {
-  let input = readFileSync('input.txt', 'utf8');
+export function meetsCriteria(passwordStr: string): boolean {
+  let password = passwordStr.split('').map(number => parseInt(number, 10));
+  if (password.length != 6) return false;
 
+  // Two adjacent digits are the same and digits never decrease.
+  let hasTwoAdjacentDigits = false;
+  for (let i = 1; i < password.length; i++) {
+    if (password[i] === password[i - 1]) {
+      hasTwoAdjacentDigits = true;
+    }
+    if (password[i] < password[i - 1]) {
+      return false;
+    }
+  }
+
+  return hasTwoAdjacentDigits;
+}
+
+export function meetsCriteria2(passwordStr: string): boolean {
+  let password = passwordStr.split('').map(number => parseInt(number, 10));
+  if (password.length != 6) return false;
+
+  // Two adjacent digits are the same and digits never decrease.
+  let hasTwoAdjacentDigits = false;
+  for (let i = 1; i < password.length; i++) {
+    if (password[i] === password[i - 1]) {
+      if (password.filter(number => number === password[i]).length == 2) {
+        hasTwoAdjacentDigits = true;
+      }
+    }
+    if (password[i] < password[i - 1]) {
+      return false;
+    }
+  }
+
+  return hasTwoAdjacentDigits;
+}
+
+function main() {
   // Part 1.
-  console.log(`part 1: `);
+  let passwordCount = 0;
+  for (var i = 124075; i <= 580769; i++) {
+    if (meetsCriteria(`${i}`)) passwordCount++;
+  }
+  console.log(`part 1: ${passwordCount}`);
 
   // Part 2.
-  console.log(`part 2: `);
+  passwordCount = 0;
+  for (var i = 124075; i <= 580769; i++) {
+    if (meetsCriteria2(`${i}`)) passwordCount++;
+  }
+  console.log(`part 2: ${passwordCount}`);
 }
 
 if (require.main === module) main();
--- 2019/day-04/test.ts
+++ 2019/day-04/test.ts
@@ -1,14 +1,30 @@
-import {} from './solution';
+import { meetsCriteria, meetsCriteria2 } from './solution';
 import { expect } from 'chai';
 
 describe('part 1', function() {
-  it('', function() {
-    expect(true).to.equal(true);
+  it('111111 meets criteria', function() {
+    expect(meetsCriteria('111111')).to.equal(true);
+  });
+
+  it("223450 doesn't meet criteria", function() {
+    expect(meetsCriteria('223450')).to.equal(false);
+  });
+
+  it("123789 doesn't meet criteria", function() {
+    expect(meetsCriteria('123789')).to.equal(false);
   });
 });
 
 describe('part 2', function() {
-  it('', function() {
-    expect(true).to.equal(true);
+  it('112233 meets criteria', function() {
+    expect(meetsCriteria2('112233')).to.equal(true);
+  });
+
+  it("123444 doesn't meets criteria", function() {
+    expect(meetsCriteria2('123444')).to.equal(false);
+  });
+
+  it('111122 meets criteria', function() {
+    expect(meetsCriteria2('111122')).to.equal(true);
   });
 });
