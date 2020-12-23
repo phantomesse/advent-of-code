a3585922ea6d5caa391ec1fd128058087fc27d9b
Lauren Zou <zou.lauren@gmail.com>
2020-12-23 14:34:57 -0500

    Add day 10 part 1.

--- 2020/day-10/solution.js
+++ 2020/day-10/solution.js
@@ -1,4 +1,16 @@
-const { readNumbers } = require('../utils/utils');
+const { readNumbers, numberSort, printPart1 } = require('../utils/utils');
 
-const input = readNumbers('test-input.txt');
-console.log(input);
+const adapters = readNumbers();
+adapters.sort(numberSort);
+adapters.unshift(0);
+adapters.push(adapters.slice(-1)[0] + 3);
+const differenceToCountMap = {};
+for (let i = 1; i < adapters.length; i++) {
+  const difference = adapters[i] - adapters[i - 1];
+  if (difference in differenceToCountMap) {
+    differenceToCountMap[difference]++;
+  } else {
+    differenceToCountMap[difference] = 1;
+  }
+}
+printPart1(differenceToCountMap[1] * differenceToCountMap[3]);
